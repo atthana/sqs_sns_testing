658412bf2516709bdc13bc44ee9fce7b8f1f661e
atthana phiphatthanaloetmongkhon <atthana.p@codium.co>
2026-06-24 01:07:40 +0700

    config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+AWS_ACCESS_KEY_ID = 'xx'
+AWS_SECRET_ACCESS_KEY = 'yy'
+AWS_REGION = 'ap-southeast-7'
+
+SNS_TOPIC_ARN = 'arn:aws:sns:ap-southeast-7:154230581564:TestOrderCreatedTopic'
+SQS_QUEUE_URL = 'https://sqs.ap-southeast-7.amazonaws.com/154230581564/TestInventoryQueue'
